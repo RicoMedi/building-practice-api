5b6a0a602097cb983670bd0ff16902eac17050c8
Madcodes831 <r.medina2119@gmail.com>
2023-11-02 20:00:29 -0700

    built first couple end points

--- api/server.js
+++ api/server.js
@@ -1,3 +1,28 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
+const Users = require( './users/model' );
+const server = express();
+server.use(express.json());
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.get('/api/users', (req, res)=>{
+    Users.find()
+        .then(user=>{
+            res.status(200).json(user);
+        })
+        .catch(err =>{
+            res.status(500).json({
+                message: "The users information could not be retrieved",
+                err: err.message,
+            })
+        })
+})
+
+
+
+server.use("*", (req, res) => {
+  res.status(404).json({
+    message: "oooops",
+  });
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
